53a617dbf47f0902f91cb165ce3ea72e04a34062
Mark W. Kidd <mark@stardart.net>
2024-10-23 22:04:13 -0400

    exercise 2
    exercise 2

--- PetStoreBuddy.cs
+++ PetStoreBuddy.cs
@@ -8,13 +8,22 @@ namespace PetStore
         static void Main()
         {
             string? userInput = String.Empty;
+            ProductLogic logic = new ProductLogic();
+
+            Console.WriteLine("***PET STORE BUDDY***");
             do
             {
-                Console.WriteLine("Type '1' to add a product");
-                Console.WriteLine("Type 'exit' to quit");
+                Console.Write("\n");
+                Console.WriteLine("Enter '1' to add a product");
+                Console.WriteLine("Enter '2' to search for a dog leash");
+                Console.WriteLine("Enter '8' to lits all products");
+                Console.WriteLine("Enter 'exit' to quit\n");
+                Console.Write("> ");
 
                 userInput = Console.ReadLine();
                 if (userInput == null) return;
+                Console.Write("\n\n");
+
                 if (userInput == "1")
                 {
                     DogLeash testProduct = new DogLeash();
@@ -50,10 +59,35 @@ namespace PetStore
                         continue;
                     }
 
-                    Console.WriteLine(JsonSerializer.Serialize(testProduct));
+                    logic.AddProduct(testProduct);
+                    Console.WriteLine($"Product: {testProduct.Name} has been added to inventory.");
+
+                } else if (userInput == "2")
+                {
+                    DogLeash? retrieved = null;
+                    Console.Write("Product search\nEnter the name of the dog leash: ");
+                    userInput = Console.ReadLine();
+                    if (userInput != null) retrieved = logic.GetDogLeashByName(userInput.Trim());
+                    if (retrieved == null)
+                    {
+                        Console.Write("Product not found.");
+                    }
+                    Console.WriteLine("Product found.");
+                    Console.WriteLine(JsonSerializer.Serialize(retrieved));
+
+
+                }
+                else if (userInput == "8")
+                {
+                    List<Product> products = logic.GetAllProducts();
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine(JsonSerializer.Serialize(product));
+                    }
                 }
             } while (userInput != null && userInput.ToLower() != "exit"); // testing for null here only to silence a warning
 
         }
     }
 }
+
---
+++ ProductLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    internal class ProductLogic
+    {
+        List<Product> _products;
+        Dictionary<string, DogLeash> _leashes;
+        Dictionary<string, CatFood> _food;
+
+        public ProductLogic()
+        {
+            _products = new List<Product>();
+            _leashes = new Dictionary<string, DogLeash>();
+            _food = new Dictionary<string, CatFood>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (product == null) throw new ArgumentNullException("Parameter 'product' cannot be null");
+            _products.Add(product);
+            if (product is DogLeash) _leashes.Add(product.Name, product as DogLeash);
+            if (product is CatFood) _food.Add(product.Name, product as CatFood);
+        }
+
+        public List<Product> GetAllProducts() { return _products; }
+
+        public DogLeash GetDogLeashByName(string name)
+        {
+            return _leashes[name];
+        }
+    }
+
+}
